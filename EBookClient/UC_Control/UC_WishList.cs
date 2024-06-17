f99b99c8f4b677262965d471c63f36584d802c9d
SOLODILLYA <31259111+SOLODILLYA@users.noreply.github.com>
2024-06-17 19:16:52 +0200

    WhishList Fixed

--- EBookClient/UC_Control/UC_WishList.cs
+++ EBookClient/UC_Control/UC_WishList.cs
@@ -24,10 +24,9 @@ namespace EBookClient.UC_Control
         private int booksPerPage = 12;
         private UC_Book[] booksElements;
 
-        static List<Author> authors = GenerateRandomAuthors(17);
-        static List<Book> allBooks = GenerateRandomBooks(52, authors);
+        static List<Book> allBooks;
         static List<Book> books = allBooks;
-        static List<Image> icons = CreateIconArray(books.Count);
+        static List<Image> icons;
         private JSONSender _jsonsender; 
         int currentUserId = 2;//TODO delete later
         public IPAddress AddrDTO { get; set; }
@@ -86,23 +85,30 @@ namespace EBookClient.UC_Control
 
         private void buttonBack_Click(object sender, EventArgs e)
         {
-
+            if (currentPage > 0)
+            {
+                currentPage = 0;
+                ShowCurrentPage();
+            }
         }
 
 
         private void buttonNext_Click(object sender, EventArgs e)
         {
-
+            currentPage++;
+            ShowCurrentPage();
         }
 
         private void buttonPreviousPage_Click(object sender, EventArgs e)
         {
-
+            currentPage--;
+            ShowCurrentPage();
         }
 
         private void buttonNextFast_Click(object sender, EventArgs e)
         {
-
+            currentPage = totalPages - 1;
+            ShowCurrentPage();
         }
 
         private void SearchField_TextChanged(object sender, EventArgs e)
@@ -153,7 +159,6 @@ namespace EBookClient.UC_Control
 
         private void UC_WhishList_Load(object sender, EventArgs e)
         {
-            ShowCurrentPage();
         }
 
         private void FiltersButton_Click(object sender, EventArgs e)
@@ -196,7 +201,6 @@ namespace EBookClient.UC_Control
                 if (serverMessage2.Messagge == "WHISHLIST_BOOKS:OK")
                 {
                     books = serverMessage2.AllBooks;
-                    MessageBox.Show($"{serverMessage2.AllBooks.Count}");
                     allBooks = serverMessage2.AllBooks;
                     icons = CreateIconArray(books.Count);
                     ShowCurrentPage();
@@ -212,84 +216,10 @@ namespace EBookClient.UC_Control
             {
                 MessageBox.Show($"Нажаль сталась помилка:{ex.Message}", "Повідомлення", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
+            icons = CreateIconArray(books.Count);
         }
 
         //TODO: Delete Later
-        public static List<Book> GenerateRandomBooks(int count, List<Author> authors)
-        {
-            var random = new Random();
-            var books = new List<Book>();
-
-            var sampleBookNames = new List<string>
-            {  
-
-                "The Great Adventure", "Mystery of the Night", "Journey to the Unknown", "The Last Frontier", "Echoes of the Past",
-                "Whispers of the Wind", "Shadows in the Moonlight", "The Enchanted Forest", "Beyond the Horizon", "Silent Thunder",
-                "The Lost Kingdom", "Rise of the Phoenix", "Secrets of the Deep", "The Time Traveler's Diary", "Legends of the Ancient",
-                "Guardian of the Realm", "The Hidden Path", "Voice of the Sea", "The Eternal Quest", "Realm of Dreams",
-                "The Forgotten Temple", "Echoes in the Valley", "Winds of Change", "Beneath the Surface", "The Crystal Cave"
-            };
-
-            var sampleAlterNames = new List<string>
-            {
-                "Adventure Series", "Night Tales", "Unknown Chronicles", "Frontier Stories", "Past Echoes",
-                "Wind Whispers", "Moonlight Shadows", "Forest Enchantment", "Horizon Beyond", "Thunder Silence",
-                "Kingdom Lost", "Phoenix Rising", "Deep Secrets", "Traveler's Tales", "Ancient Legends",
-                "Realm Guardians", "Path Hidden", "Sea Voices", "Quest Eternal", "Dream Realms",
-                "Temple Forgotten", "Valley Echoes", "Change Winds", "Surface Depths", "Cave Crystals"
-            };
-
-            for (int i = 0; i < count; i++)
-            {
-                var author = authors[random.Next(authors.Count)];
-                var book = new Book
-                {
-                    Id = i + 1,
-                    GenreId = random.Next(1, 10),
-                    CategoryId = random.Next(1, 10),
-                    AuthorId = author.Id,
-                    BookName = sampleBookNames[random.Next(sampleBookNames.Count)],
-                    AlterName = sampleAlterNames[random.Next(sampleAlterNames.Count)],
-                    Published = DateTime.Now.AddDays(-random.Next(0, 3650)), // Random date within the last 10 years
-                    PublisherId = random.Next(1, 10),
-                    Publisher = new Publisher(),
-                    Genre = new Genre(),
-                    Category = new Category(),
-                    Author = author,
-                    Reviews = new List<Review>(),
-                    Transactions = new List<Transaction>()
-                };
-                books.Add(book);
-            }
-            return books;
-        }
-
-        public static List<Author> GenerateRandomAuthors(int count)
-        {
-            var random = new Random();
-            var authors = new List<Author>();
-
-            var sampleAuthorNames = new List<string>
-            {
-                "John Smith", "Emily Johnson", "Michael Williams", "Sarah Jones", "David Brown",
-                "Laura Davis", "James Wilson", "Linda Martinez", "Robert Taylor", "Barbara Anderson",
-                "Daniel Thomas", "Jennifer Lee", "Matthew Harris", "Elizabeth Clark", "Andrew Lewis",
-                "Jessica Walker", "Joshua Robinson", "Sophia Hall", "William Young", "Olivia King"
-            };
-
-            for (int i = 0; i < count; i++)
-            {
-                authors.Add(new Author
-                {
-                    Id = i + 1,
-                    AuthorName = sampleAuthorNames[random.Next(sampleAuthorNames.Count)],
-                    Rate = (float)Math.Round(random.NextDouble() * 5, 2),
-                    
-                });
-            }
-
-            return authors;
-        }
         public static List<Image> CreateIconArray(int numberOfIcons)
         {
             List<Image> images = new List<Image>();
