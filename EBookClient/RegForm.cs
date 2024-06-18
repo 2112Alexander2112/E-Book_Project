7ceaf2b5a67253fd19b3e2b92f0c470cc4294b57
SOLODILLYA <31259111+SOLODILLYA@users.noreply.github.com>
2024-06-18 12:41:20 +0200

    RegisterUpdated

--- EBookClient/RegForm.cs
+++ EBookClient/RegForm.cs
@@ -14,6 +14,8 @@ using static System.Windows.Forms.VisualStyles.VisualStyleElement.ListView;
 using EBookLib01.HelperModels;
 using EBookLib01.HelperModels.TransitModels;
 using System.IO;
+using System.Globalization;
+using System.Text.RegularExpressions;
 
 namespace EBookClient
 {
@@ -66,9 +68,9 @@ namespace EBookClient
             }
             else
             {
-                if(string.IsNullOrWhiteSpace(emai) )
+                if(string.IsNullOrWhiteSpace(emai) || !IsValidEmail(emai))
                 {
-                    label2.Text = "Ви не заповнили поле Email!";
+                    label2.Text = "Ви неправильно заповнили поле Email!";
                     timer1.Start();
                 }
                 else
@@ -80,9 +82,9 @@ namespace EBookClient
                     }
                     else
                     {
-                        if(string.IsNullOrWhiteSpace(pass) )
+                        if(string.IsNullOrWhiteSpace(pass) || !IsValidPassword(pass))
                         {
-                            label4.Text = "Ви не заповнили поле Паролю!";
+                            label4.Text = "Ви неправильно заповнили поле Паролю!";
                             timer1.Start();
                         }
                         else
@@ -154,5 +156,62 @@ namespace EBookClient
         {
             this.DialogResult = DialogResult.Cancel;
         }
+        public static bool IsValidEmail(string email)
+        {
+            if (string.IsNullOrWhiteSpace(email))
+            {
+                return false;
+            }
+
+            try
+            {
+                email = Regex.Replace(email, @"(@)(.+)$", DomainMapper,
+                                      RegexOptions.None, TimeSpan.FromMilliseconds(200));
+
+                string DomainMapper(Match match)
+                {
+                    var idn = new IdnMapping();
+
+                    string domainName = idn.GetAscii(match.Groups[2].Value);
+
+                    return match.Groups[1].Value + domainName;
+                }
+            }
+            catch (RegexMatchTimeoutException)
+            {
+                return false;
+            }
+            catch (ArgumentException)
+            {
+                return false;
+            }
+
+            try
+            {
+                return Regex.IsMatch(email,
+                    @"^[^@\s]+@[^@\s]+\.[^@\s]+$",
+                    RegexOptions.IgnoreCase, TimeSpan.FromMilliseconds(250));
+            }
+            catch (RegexMatchTimeoutException)
+            {
+                return false;
+            }
+        }
+        public static bool IsValidPassword(string password)
+        {
+            if (string.IsNullOrWhiteSpace(password))
+            {
+                return false;
+            }
+
+            bool hasUpperCase = password.Any(char.IsUpper);
+            bool hasLowerCase = password.Any(char.IsLower);
+            bool hasDigit = password.Any(char.IsDigit);
+            bool hasSpecialChar = password.Any(ch => !char.IsLetterOrDigit(ch));
+            bool hasMinLength = password.Length >= 8;
+
+            return hasUpperCase && hasLowerCase && hasDigit && hasSpecialChar && hasMinLength;
+        }
+
     }
 }
